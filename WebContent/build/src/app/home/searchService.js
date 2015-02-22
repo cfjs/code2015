0fd6b1bbd02a434dc00df413b89ce363c4d81879
Code-edoc <jsthivierge@ubuntu.(none)>
2015-02-21 16:23:19 -0800

    Removed build folder from git

--- WebContent/build/src/app/home/searchService.js
+++
@@ -1,8 +0,0 @@
-angular.module('home.services', [])
-	.factory('searchService', function(searchCriteria) {
-		return {
-			performSearch: function(searchCriteria) {
-				console.log(searchCriteria);
-			}
-		};
-	});
